e6eec8b5d447b32c0d76bba9023bbd7ff9a2fe8e
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-12-20 22:08:33 -0500

    Began implementation of overarching GameManager
    Decorate Tree scene interacts with GameManager
    Gain score/lose lives as games are won/lost

--- Jame Gam Christmas Edition/Assets/Scripts/BetweenGamesManager.cs
+++ Jame Gam Christmas Edition/Assets/Scripts/BetweenGamesManager.cs
@@ -13,15 +13,48 @@ public class BetweenGamesManager : MonoBehaviour
     [SerializeField] GameObject santa;
     [SerializeField] List<GameObject> hats;
 
+    // References
+    private GameManager gameManager;
+    private Animator animator;
+
+    // Variables
+    private int numLives;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        animator = GameObject.Find("Santa").GetComponent<Animator>();
+
+        numLives = gameManager.GetLives();
+
+        if (numLives == 3)
+        {
+            hats[3].SetActive(false);
+        }
+        if (numLives == 2)
+        {
+            hats[3].SetActive(false);
+            hats[2].SetActive(false);
+        }
+        if (numLives == 1)
+        {
+            hats[3].SetActive(false);
+            hats[2].SetActive(false);
+            hats[1].SetActive(false);
+        }
+        if (numLives == 0)
+        {
+            hats[3].SetActive(false);
+            hats[2].SetActive(false);
+            hats[1].SetActive(false);
+            hats[0].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetInteger("Game Result", gameManager.GetGameResult());
     }
 }
