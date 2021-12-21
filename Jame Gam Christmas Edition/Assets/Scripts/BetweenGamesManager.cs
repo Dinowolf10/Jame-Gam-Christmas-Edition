d6cc3b111bdacdc7e431bfdbc589d09eb37bfa7b
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-12-20 19:33:15 -0500

    Created scene for in between mini games
    Imported art assets for in between games and decorating tree

---
+++ Jame Gam Christmas Edition/Assets/Scripts/BetweenGamesManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// file: BetweenGamesManager.cs
+/// description: Manages the state of the Between Games scene
+/// author: Nathan Ballay
+/// </summary>
+public class BetweenGamesManager : MonoBehaviour
+{
+    // Objects
+    [SerializeField] GameObject santa;
+    [SerializeField] List<GameObject> hats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
